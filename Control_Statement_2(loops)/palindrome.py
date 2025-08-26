49a3dd2ae027bd5585e2efc7325f6a06d18a486b
Deepak-vash23 <deepakvashisth8282@gmail.com>
2025-08-26 13:01:08 +0530

    Update palindrome.py

--- Control_Statement_2(loops)/palindrome.py
+++ Control_Statement_2(loops)/palindrome.py
@@ -1,16 +1,9 @@
 A = int(input("Enter an integer: "))
 
-if A < 0:
-    print("Cannot check palindrome for negative integers.")
-else:
-    original = A
-    reverse = 0
-    while A > 0:
-        digit = A % 10
-        reverse = reverse * 10 + digit
-        A = A // 10
+a = str(A)
+b = a[::-1]
 
-    if original == reverse:
-        print("Yes, it is a palindrome.")
-    else:
-        print("No, it is not a palindrome.")
+if a == b:
+    print("The number is a palindrome")
+else:
+    print("The number is not a palindrome")
