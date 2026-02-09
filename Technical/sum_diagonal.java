540657592cdf6143851f2f274697a7754f4651ee
[Deepak Vashisth] <[deepakvashisth8282@gmail.com]>
2026-02-09 14:36:59 +0530

    Added sum of diagonal

---
+++ Technical/sum_diagonal.java
@@ -0,0 +1,36 @@
+//WAP to find sum of diagonal of matrix
+
+import java.util.Scanner;
+
+public class sum_diagonal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows and cols");
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        int[][] matrix = new int[row][col];
+        //for input and sum together
+        int sum=0;
+        System.out.println("Enter matrix element: ");
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
+                matrix[i][j] = sc.nextInt();
+                if(i==j){
+                    sum +=matrix[i][j];
+                }
+            }
+        }
+        System.out.println("Matrix is: ");
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Sum of diagonal element is: " + sum);
+
+
+    }
+}
